2bad272e5eda854176cf7ce4b93f834675de49a4
Siddharth <siddharth.anand@shorthills.ai>
2024-09-08 14:18:48 +0530

    First Commit

---
+++ codesample.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void print(int *arr,int n){
+    
+    for(int i=0;i<5;i++){
+        arr[i]=arr[i]*arr[i];
+    }
+    sort(arr,arr+5);
+}
+int main()
+{
+    int arr[]={-2,-1,3,5,7};
+    print(arr,5);
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
